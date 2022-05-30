96c966434bde84fcecf9594cfc1efaf22f43baf8
V Zhevaha <v.zhevaga@gmail.com>
2022-05-30 23:06:52 +0200

    mobile adaptive

---
+++ js/app.js
@@ -0,0 +1,8 @@
+console.log("JS start!");
+
+import * as flsFunctions from "./files/functions.js";
+flsFunctions.isWebp();
+
+import * as burger from 					"./files/burger.js";
+import * as scrolls from 					"./files/scrolls.js";
+import * as submenu from 					"./files/submenu.js";
